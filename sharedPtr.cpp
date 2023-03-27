9434398aae9c8b6d041b28770a2513bbfa27a61b
fqzz2000 <fqzz2000@outlook.com>
2023-03-27 14:28:26 +0800

    first version shared and unique

---
+++ sharedPtr.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+template <typename T>
+class SharedPtr
+{
+public:
+    // constructor
+    SharedPtr(T *ptr = nullptr) : _count(nullptr), _ptr(ptr)
+    {
+        if (_ptr != nullptr)
+        {
+            std::cout << "created" << std::endl;
+            _count = new int(1);
+        }
+        return;
+    }
+    // copy constructor
+    SharedPtr(const SharedPtr &rhs) : _count(rhs._count), _ptr(rhs._ptr)
+    {
+        if (_count != nullptr)
+        {
+            ++(*_count);
+        }
+    }
+    // assignment constructor
+    SharedPtr &operator=(const SharedPtr &rhs)
+    {
+        if (&rhs != this)
+        {
+            SharedPtr tmp(rhs);
+            std::swap(_count, tmp._count);
+            std::swap(_ptr, tmp._ptr);
+        }
+        return *this;
+    }
+
+    // destructor
+    ~SharedPtr()
+    {
+        if (_count != nullptr && --(*_count) == 0)
+        {
+            std::cout << "destoryed" << std::endl;
+            delete _count;
+            delete _ptr;
+            _count = nullptr;
+            _ptr = nullptr;
+        }
+        else
+        {
+            std::cout << "count--" << std::endl;
+        }
+    }
+
+private:
+    int *_count;
+    T *_ptr;
+};
+
+int main(int argc, char **argv)
+{
+    SharedPtr<int> sp1(new int(10));
+    SharedPtr<int> sp2(new int(20));
+    SharedPtr<int> sp3(new int(30));
+    SharedPtr<int> sp4(sp1);
+    sp2 = sp1;
+    sp3 = sp1;
+
+    return 0;
+}
---
+++ uniquePtr.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+template <typename T>
+class UniquePtr
+{
+public:
+    UniquePtr(T *ptr = nullptr) : _ptr(ptr) {}
+    // copy constructor
+    // move constructor
+    UniquePtr(UniquePtr &&rhs) : _ptr(nullptr)
+    {
+        if (rhs._ptr != nullptr)
+        {
+            UniquePtr tmp(rhs);
+            std::swap(_ptr, tmp._ptr);
+        }
+    }
+    // destructor
+    ~UniquePtr()
+    {
+        std::cout << "destroyed" << std::endl;
+        if (_ptr != nullptr)
+        {
+            delete _ptr;
+            _ptr = nullptr;
+        }
+    }
+    // assignment operator
+
+private:
+    UniquePtr &operator=(const UniquePtr &rhs);
+    UniquePtr(const UniquePtr &rhs);
+
+private:
+    T *_ptr;
+};
+
+int main()
+{
+    UniquePtr<int> sp1(new int(10));
+    UniquePtr<int> sp2(new int(20));
+    // UniquePtr<int> sp3(sp1); // 错误
+    // sp1 = sp2; // 错误
+
+    return 0;
+}
